d220ad9ea8b625d66166887a01a682ded06a7ec0
Jang Eun Ji <feunji21@gmail.com>
2018-11-20 00:51:52 +0900

    First Source code

---
+++ watch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class watch
+{
+	public:
+		int hour;
+		int minute;
+		int second;
+		watch(int h, int m, int s)
+		{
+			hour=h;
+			minute=m;
+			second=s;
+		}
+
+void getTime(){
+	cout << "What time id it? " << hour << "hour " << minute << "minute " << second << "second." << endl;
+}
+};
+
+int main()
+{	watch w(3, 26, 53);
+	w.getTime();
+
+	return 0;
+}
